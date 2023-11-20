b42f433190f9fe802a0c612fe791064dd111c365
jami100YT <73913699+jami100YT@users.noreply.github.com>
2023-11-20 17:00:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+import discord
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+# setting up the bot
+intents = discord.Intents.all() 
+# if you don't want all intents you can do discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+# sync the slash command to your server
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=1085581168486912050))
+    # print "ready" in the console when the bot is ready to work
+    print("ready")
+
+# make the slash command
+@tree.command(name="hello", description="description", guild=discord.Object(id=1085581168486912050))
+async def slash_command(interaction: discord.Interaction):    
+    await interaction.response.send_message("Hello World")
+
+# run the bot
+client.run(TOKEN)
